c1e627f7787d5dd733609f95e8d911403d96c851
Piiu <piiupilt@silktide.com>
2017-02-16 22:13:03 +0000

    settings page and help

--- wp-github-release-listener.php
+++ wp-github-release-listener.php
@@ -181,42 +181,90 @@ function wgrl_register_settings() {
 }
 
 function wgrl_options_page() {
-    echo '<div class="wrap">';
-    echo '<h2>GitHub release listener settings</h2>';
-    echo '<form method="post" action="options.php" style="text-align: left;">';
-    settings_fields('wgrl-options');
-    do_settings_sections( 'wgrl-options' );
-    echo '<table>';
-    echo '    <tr>';
-    echo '        <th>Webhook secret</th>';
-    echo '            <td><input type="password" name="wgrl-webhook-secret" value="'. esc_attr( get_option('wgrl-webhook-secret') ) .'" required /></td>';
-    echo '    </tr>';
-    echo '    <tr>';
-    echo '        <th>Assign posts to user</th>';
-    echo '        <td>'. wp_dropdown_users(['name' => 'wgrl-post-author', 'echo' => false, 'selected' => get_option('wgrl-post-author') ]). '</td>';
-    echo '    </tr>';
-    echo '    <tr>';
-    echo '        <th>Post type</th>';
-    echo '        <td>';
-    echo '            <select name="wgrl-custom-post-type">';
-    echo '                <option value="0">Post</option>';
-    echo '                <option value="1" '.(get_option('wgrl-custom-post-type') ? 'selected' : '').'>Release</option>';
-    echo '            </select>';
-    echo '        </td>';
-    echo '    </tr>';
-    echo '    <tr>';
-    echo '        <th>Tag post (only for post type post)</th>';
-    echo '       <td><input type="text" name="wgrl-tag-post" value="'. wgrl_get_custom_tag() .'" required /></td>';
-    echo '    </tr>';
-    echo '    <tr>';
-    echo '        <th>Webhook callback URL</th>';
-    echo '        <td><code>'. esc_url(admin_url('admin-ajax.php')) . '?action=wgrl_release_post</code></td>';
-    echo '    </tr>';
-    echo '</table>';
-    submit_button();
-    echo '</form>';
-    echo '</div>';
-    echo '<script>';
+    ?>
+    <div class="wrap">
+        <h1>GitHub release listener setup</h1>
+        <form method="post" action="options.php" style="text-align: left;">
+            <?php settings_fields('wgrl-options'); ?>
+            <?php do_settings_sections( 'wgrl-options' ); ?>
+            <table class="form-table">
+                <tr>
+                    <th>Secret</th>
+                        <td><input type="password" name="wgrl-webhook-secret" value="<?= esc_attr( get_option('wgrl-webhook-secret') ) ?>" required /></td>
+                </tr>
+                <tr>
+                    <th>Payload URL</th>
+                    <td><code><?= esc_url(admin_url('admin-ajax.php')) ?>?action=wgrl_release_post</code></td>
+                </tr>
+            </table>
 
-    echo '</script>';
+            <a href="#" id="wgrl_help_link" onClick="showHelp()">How do I use this?</a>
+            <div id="wgrl_help" class="card" style="display:none;">
+                <h3>Connecting with GitHub</h3>
+                <ol>
+                    <li>Go to your project settings on GitHub</li>
+                    <li>Select Webhooks from the menu</li>
+                    <li>Click "Add webhook" (top right)</li>
+                    <li>Copy payload URL from here to GitHub</li>
+                    <li>Select "application/json" as content type</li>
+                    <li>Create a passcode (a random string) and copy it to "Secret" field on both here and GitHub</li>
+                    <li>Choose "Let me select individual events" as triggers</li>
+                    <li>Tick "Release" and untick everything else</li>
+                    <li><strong>Save your plugin settings</strong></li>
+                    <li>Click "Add webhook" on GitHub</li>
+                </ol>
+                GitHub sends a ping to your payload URL on webhook activation. It can be found under Recent Deliveries and details will open upon clicking on the most recent one.
+                In case the activation was successful the Response tab should read 200 and response body should be <code>{"success":true,"release_published":false}</code>.
+                Nothing will be published on your site before an actual release is made on GitHub.
+            </div>
+
+            <table class="form-table">
+                <tr>
+                    <th>Assign posts to user</th>
+                    <td>
+                        <?= wp_dropdown_users(['name' => 'wgrl-post-author', 'echo' => false, 'selected' => get_option('wgrl-post-author') ]) ?>
+                        <p class="description">User must have post create and publish capabilities</p>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Post type</th>
+                    <td>
+                        <select name="wgrl-custom-post-type" id="wgrl-custom-post-type">
+                            <option value="0">Post</option>
+                            <option value="1" <?= (get_option('wgrl-custom-post-type') ? 'selected' : '') ?>>Release (custom post type)</option>
+                        </select>
+                        <p class="description">Choose Release if you do not wish to list releases with your other posts</p>
+                    </td>
+                </tr>
+                <tr id="wgrl-tag-post-tr">
+                    <th>Tag post</th>
+                   <td>
+                       <input type="text" name="wgrl-tag-post" value="<?= wgrl_get_custom_tag() ?>" required />
+                       <p class="description">Tag is used to list posts containing release notes, e.g. using shortcode</p>
+                   </td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <script>
+        var option = jQuery( "#wgrl-custom-post-type" );
+        var tagLine = jQuery('#wgrl-tag-post-tr');
+        var helpLink = jQuery('#wgrl_help_link');
+        var help = jQuery('#wgrl_help');
+        option.change(function() {toggleTagline()});
+        function toggleTagline() {
+            if ( option.val() == 1 ) {
+                tagLine.hide();
+            } else {
+                tagLine.show();
+            }
+        }
+        function showHelp() {
+            help.show();
+            helpLink.hide();
+        }
+        toggleTagline();
+    </script>
+    <?php
 }
